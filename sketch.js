818fea30324c283ebd48e38c1a1f99a4ec52a633
jasondykstra <jasondykstra2000@gmail.com>
2022-03-13 23:18:42 -0400

    followed tutorial for handwritten digit recognition

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+import { MnistData } from "./data.js";
+
 const len = 784;
 const total_data = 1000;
 const canvas_width = 400;
@@ -31,6 +33,179 @@ function preload() {
   }
 }
 
+async function showExamples(data){
+  // Create container
+  const surface = tfvis.visor().surface({name: 'Input Data Examples', tab: 'Input Data'});
+
+  // Get examples
+  const examples = data.nextTestBatch(20);
+  const numExamples = examples.xs.shape[0];
+
+  // Create canvas element to render each example
+  for (let i = 0; i < numExamples; i++){
+    const imageTensor = tf.tidy(() => {
+      // Reshape image to 28x28 px
+      return examples.xs
+        .slice([i, 0], [1, examples.xs.shape[1]])
+        .reshape([28, 28, 1]);
+    });
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 28;
+    canvas.height = 28;
+    canvas.style = 'margin: 4px;';
+    await tf.browser.toPixels(imageTensor, canvas);
+    surface.drawArea.appendChild(canvas);
+
+    imageTensor.dispose();
+  }
+}
+
+async function run() {
+  // TODO: Update this to actually store the data
+  const data = new MnistData();
+  await data.load();
+  await showExamples(data);
+
+  const model = getModel();
+  tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+  
+  await train(model, data);
+
+  await showAccuracy(model, data);
+  await showConfusion(model, data);
+
+
+}
+
+document.addEventListener('DOMContentLoaded', run);
+
+function getModel() {
+  const model = tf.sequential();
+
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const IMAGE_CHANNELS = 1;
+
+  // First layer on CNN specify input shape and parameters. 
+  model.add(tf.layers.conv2d({
+    inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+    kernelSize: 5,
+    filters: 8,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+
+  // Maxpool layer for downsampling as opposed to averaging
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+  // Repeat for another conv2d + maxpooling stack
+  // More filters in this convolution
+  model.add(tf.layers.conv2d({
+    kernelSize: 5,
+    filters: 16,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+  // Flatten output from 2d filters into 1D vector to feed into final layer
+  model.add(tf.layers.flatten());
+
+  // Last layer is dense layer which has N output units, one for each doodle we want to classify
+  const NUM_OUTPUT_CLASSES = 10;
+  model.add(tf.layers.dense({
+    units: NUM_OUTPUT_CLASSES,
+    kernelInitializer: 'varianceScaling',
+    activation: 'softmax'
+  }));
+
+  // Optimizer, loss function, accuracy metric
+  const optimizer = tf.train.adam();
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  return model;
+
+}
+
+async function train(model, data) {
+  const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+  const container = {
+    name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
+  };
+  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+  
+  const BATCH_SIZE = 512;
+  const TRAIN_DATA_SIZE = 5500;
+  const TEST_DATA_SIZE = 1000;
+
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+    return [
+      d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+      d.labels
+    ];
+  });
+
+  const [testXs, testYs] = tf.tidy(() => {
+    const d = data.nextTestBatch(TEST_DATA_SIZE);
+    return [
+      d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+      d.labels
+    ];
+  });
+
+  return model.fit(trainXs, trainYs, {
+    batchSize: BATCH_SIZE,
+    validationData: [testXs, testYs],
+    epochs: 10,
+    shuffle: true,
+    callbacks: fitCallbacks
+  });
+}
+
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+function doPrediction(model, data, testDataSize = 500) {
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax(-1);
+  const preds = model.predict(testxs).argMax(-1);
+
+  testxs.dispose();
+  return [preds, labels];
+}
+
+
+
+async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = {name: 'Accuracy', tab: 'Evaluation'};
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+  labels.dispose();
+}
+
+async function showConfusion(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  tfvis.render.confusionMatrix(container, {values: confusionMatrix, tickLabels: classNames});
+
+  labels.dispose();
+}
+
+
+
 // Splits data into training and testing sets
 function prepareData(category, data, label) {
   category.training = [];
@@ -49,6 +224,8 @@ function prepareData(category, data, label) {
     }
   }
 }
+
+
 
 function trainEpoch(training) {
   // Randomizing training data
@@ -175,28 +352,31 @@ function setup() {
   let canvas = createCanvas(canvas_width, canvas_height);
   canvas.background(0);
   canvas.parent("canvasDiv")
-  zeroGuess();
+  // zeroGuess();
 
   for (let key of objects.keys()) {
     object_sets.push({});
     prepareData(object_sets[key], object_data[key], key);
   }
 
+
+  // await train(model, data);
+
   // Make nn object, 784 inputs (one per pixel value), 64 nodes in hidden layer, x outputs for all objects
   
-  nn = new NeuralNetwork(784, 60, objects.size);
-    // nn = RandomForestClassifier(200);
+  // nn = new NeuralNetwork(784, 60, objects.size);
+  //   // nn = RandomForestClassifier(200);
 
-  // Preparing the data
-  let training = [];
-  for (let key of objects.keys()) {
-    training = training.concat(object_sets[key].training);
-  }
+  // // Preparing the data
+  // let training = [];
+  // for (let key of objects.keys()) {
+  //   training = training.concat(object_sets[key].training);
+  // }
 
-  let testing = [];
-  for (let key of objects.keys()) {
-    testing = testing.concat(object_sets[key].testing);
-  }
+  // let testing = [];
+  // for (let key of objects.keys()) {
+  //   testing = testing.concat(object_sets[key].testing);
+  // }
 
   let trainButton = select('#train');
   let epochCounter = 0;
